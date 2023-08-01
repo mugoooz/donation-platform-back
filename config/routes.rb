d8e0fa99f1aa6ed5a3c958c90debb7f5bb290c5e
Eric Mugo <mugoericpg20@gmail.com>
2023-08-01 22:29:21 +0300

    created models & controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :donations
+  resources :admins
+  resources :beneficiaries
+  resources :donors
+  resources :charities
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
