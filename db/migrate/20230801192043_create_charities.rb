796095fae918468d283dbfdf89da6a5b0ceee722
Elsie <hawiokatch@gmail.com>
2023-08-02 10:17:42 +0300

    Charities migration

--- db/migrate/20230801192043_create_charities.rb
+++ db/migrate/20230801192043_create_charities.rb
@@ -1,6 +1,12 @@
 class CreateCharities < ActiveRecord::Migration[7.0]
   def change
     create_table :charities do |t|
+      t.string :name
+      t.string :location
+      t.string :email
+      t.string :phone_number
+      t.string :password_digest
+      t.text :purpose
 
       t.timestamps
     end
